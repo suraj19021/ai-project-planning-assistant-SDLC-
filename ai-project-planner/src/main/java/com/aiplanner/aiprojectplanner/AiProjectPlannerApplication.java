f4442f680116ef93879c27d3834f3dff84ca240e
suraj.shoot@gmail.com <suraj.shoot@gmail.com>
2026-07-17 12:41:27 +0530

    Initial Spring Boot project setup

---
+++ ai-project-planner/src/main/java/com/aiplanner/aiprojectplanner/AiProjectPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.aiplanner.aiprojectplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProjectPlannerApplication.class, args);
+    }
+
+}
---
+++ ai-project-planner/src/test/java/com/aiplanner/aiprojectplanner/AiProjectPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiplanner.aiprojectplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjectPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
